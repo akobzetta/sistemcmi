6492b92c5831170ba54d8de9d455feac257e4304
Akob <akob@zetta-hotelz.com>
2016-02-24 09:49:01 +0800

    Update setup.php

--- setup.php
+++ setup.php
@@ -6,13 +6,24 @@
 <?php
 if(isset($_POST["pass"]))
 {
-	$pwd = mysqli_real_escape_string($conn, ($_POST["pwd"]);
+	$pwd = mysqli_real_escape_string($conn, ($_POST["pwd"]));
 	$password = password_hash($pwd, PASSWORD_DEFAULT);
 	mysqli_query($conn, "INSERT INTO users (username, password, rank) VALUES ('Admin', '{$password}', '3');");
 	echo "done: <br>Username: Admin<br>Password: {$pwd}<br>return_code: #dev_".$password;
 }
 ?>
 <form action="" method="post">
-<input name="pwd" type="password">
-<input name="pass" type="submit" value="go!">
+ <div class="form-group">
+	<div class="input-group">
+	<span class="input-group-addon" >Username</span>
+    <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Admin" disabled>
+	 </div>
+  </div>
+ <div class="form-group">
+	<div class="input-group">
+	<span class="input-group-addon" >Password</span>
+    <input type="password" class="form-control" id="pwd" name="pwd">
+	 </div>
+  </div>
+<input name="pass" type="submit" value="Setup!" class='btn btn-success'>
 </form>
